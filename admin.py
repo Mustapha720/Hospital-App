3b02f1363d6a845b14547ec638fe43264c7dd991
Mustapha Oluwasarasimi <mustaphaoluwadarasimi5@gmail.com>
2025-09-16 12:34:36 +0100

    Enhance patient sign-up process with error handling and add main menu structure

--- admin.py
+++ admin.py
@@ -32,11 +32,18 @@ class User():
 
     def signUpPatient(self):
         p_name = input("Fullname: ")
-        p_age = int(input("Age: "))
+        try:
+            p_age = int(input("Age: "))
+        except ValueError:
+            print("Invalid age! Please enter a number.")
+            return
         p_gender = input("Gender: ")
         p_email = input("E-mail: ")
-        p_contact = int(input("Contact: "))
-        p_emergency_contact = int(input("Emergency Contact: "))
+        try:
+            p_contact = int(input("Contact: "))
+            p_emergency_contact = int(input("Emergency Contact: "))
+        except ValueError:
+            print("Contact must be numbers!")
         p_blood_group = input("Blood Group (e.g A+, A-, B+, B-, O+, O-, AB+, AB-): ")
 
         patient_info = {
@@ -63,22 +70,83 @@ class User():
     def logout(self):
         pass
 
+    def main_menu(self):
+        while True:
+            print("""
+                    -------MAIN MENU-------
+                    1. Admin / Staff Login
+                    2. Doctor Login
+                    3. Patient Login
+                    4. Exit
+                """)
+            option = input("Select an option(1-4): ")
+            if option == '1':
+                pass
+            elif option == '2':
+                doc = Doctor(self.user_id, self.name, "Doctor", self.email)
+                doc.doc_menu()
+            elif option == '3':
+                pass
+            elif option == '4':
+                print("Thanks for visiting our hospital.....")
+                break
+            else:
+                print("Inb=valid option, try again!")
+
 class Patient(User):
-    pass
+    def patient_menu(self):
+        print("""
+                -------PATIENT MENU-------
+                1. Book Appointment
+                2. View Appointments
+                3. View Bills
+                4. View Medical History
+                5. Back to Main Menu
+            """)
 
 class Doctor(User):
-    pass
+    def doc_menu(self):
+        print("""
+                -------DOCTOR MENU-------
+                1. View Patients
+                2. View Schedule
+                3. Update Availability
+                4. Back to Main Menu
+            """)
 
-class Appointment(User):
-    pass
+class Appointment():
+    def appointment_menu(self):
+        print("""
+                -------APPOINTMENT MENU-------
+                1. Book Appointment
+                2. View All APpointments
+                3. Search Appointment (patient ID / doc ID)
+                4. Reschedule Appointment
+                5. Cancel Appointment
+                6. Back to Hospital Menu
+            """)
 
-class Payment(User):
-    pass
+class Payment():
+    def payment_menu(self):
+        print("""
+                -------PAYMENT MENU-------
+                1. Make Payment
+                2. View Payment History
+                3. Back to Main Menu
+            """)
 
-class Hospital(User):
-    pass
+class Hospital():
+    def hospital_menu(self):
+        print("""
+                -------HOSPITAL MENU-------
+                1. Doctor Management
+                2. Patient Management
+                3. Appointment Management
+                4. Billing & Payments
+                5. Back to Main Menu
+            """)
 
-end = User(1, 'Dara',  'Dentist', 'fm@gmail.com')
+end = User(1, 'Dara',  'Doctor', 'fm@gmail.com')
 end.signUpPatient()
 # print(end.to_dict())
 # print(end.name)
